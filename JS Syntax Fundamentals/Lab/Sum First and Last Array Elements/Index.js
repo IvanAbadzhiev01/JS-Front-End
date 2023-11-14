6c922b96d9301fc4b46a484ca5f84a536124f17c
IvanAbadjiev <ivan087611@gmail.com>
2023-11-14 22:34:57 +0200

    JS Syntax Fundamentals
    • JavaScript Syntax;
    • Conditional Statements;
    • Loops (while and for Loops);
    • Data Types and Variables;
    • Arrays;
    • String Processing;
    • Debugging.

---
+++ JS Syntax Fundamentals/Lab/Sum First and Last Array Elements/Index.js
@@ -0,0 +1,8 @@
+function SumFirstAndLastElement(arr){
+    let firstElement = arr[0];
+    let lastElement = arr[arr.length-1];
+
+    console.log(firstElement + lastElement);
+}
+
+SumFirstAndLastElement([10, 11 , 13 , 100 , 5]);
