192c0ab0bb5dc9e4e4d1b71c280e4ed087e174f1
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2023-11-24 09:04:52 +0200

    Add task
    Functions and Statements
    
    • Declaring and Invoking Functions;
    • Nested Functions;
    • Value vs Reference Types;
    • First-class Functions;
    • Arrow Functions;
    • Naming and Best Practices.

---
+++ Functions and Statements/Exercise/P02AddAndSubtract/index.js
@@ -0,0 +1,9 @@
+
+function sum(numberOne, numberTwo, numberThree){
+    console.log(numberOne + numberTwo - numberThree);
+}
+function substract(numberOne, numberTwo)
+{
+    return numberOne - numberTwo;
+}
+
