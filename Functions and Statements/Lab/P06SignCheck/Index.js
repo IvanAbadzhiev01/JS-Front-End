58854ff662359ea4dffc3643f2c10cb2a7ffb4e6
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-01 10:58:54 +0200

    Add new task
    Functions and Statements
    
    • Declaring and Invoking Functions;
    • Nested Functions;
    • First-class Functions;
    • Arrow Functions;
    • Naming and Best Practices.

---
+++ Functions and Statements/Lab/P06SignCheck/Index.js
@@ -0,0 +1,11 @@
+function signCheck(num1, num2, num3){
+    const result = num1 * num2 * num3;
+
+    if(result < 0){
+        console.log("Negative");
+    }else{
+        console.log("Positive");
+    }
+
+
+}
