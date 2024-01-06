3cf32077239ad9bf7d3c603a8a9a02757d8375bd
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-06 09:40:47 +0200

    Add new task
    Objects and Classes
    
    • Objects (definition, properties, and methods);
    • Associative Arrays;
    • JSON;
    • ES6 Classes.

---
+++ Objects and Classes/Lab/P01PersonInfo/index.js
@@ -0,0 +1,11 @@
+function printPersonInfo(firstName, lastName, age) {
+  let person = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+  };
+
+  return person;
+}
+
+printPersonInfo("Peter", "Pan", 20);
