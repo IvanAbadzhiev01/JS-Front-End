3a7d7209035e8e3cf3029e9510b9fd128982db2c
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-07 10:50:58 +0200

    Add new task
    Objects and Classes
    
    • Objects (definition, properties, and methods);
    • Associative Arrays;
    • JSON;
    • ES6 Classes.

---
+++ Objects and Classes/Lab/P04ConvertToJSON/index.js
@@ -0,0 +1,11 @@
+function solve(firstName, lastName, hairColor) {
+  const person = {
+    name: firstName,
+    lastName: lastName,
+    hairColor: hairColor,
+  };
+
+  console.log(JSON.stringify(person));
+}
+
+solve(`George`, `Jones`, `Brown`);
