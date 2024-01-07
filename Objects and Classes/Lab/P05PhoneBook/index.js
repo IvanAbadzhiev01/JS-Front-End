6ef46d9eed7a8757b752d1d12e338f4a431e788f
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-07 10:51:07 +0200

    Add new task
    Objects and Classes
    
    • Objects (definition, properties, and methods);
    • Associative Arrays;
    • JSON;
    • ES6 Classes.

---
+++ Objects and Classes/Lab/P05PhoneBook/index.js
@@ -0,0 +1,19 @@
+function collectPhones(arr) {
+  const phoneBook = arr.reduce((acc, curr) => {
+    const [name, phone] = curr.split(" ");
+    acc[name] = phone;
+
+    return acc;
+  }, {});
+
+  Object.entries(phoneBook).forEach(([key, value]) => {
+    console.log(`${key} -> ${value}`);
+  });
+}
+
+collectPhones([
+  "Tim 0834212554",
+  "Peter 0877547887",
+  "Bill 0896543112",
+  "Tim 0876566344",
+]);
