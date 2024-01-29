f0249ad511b6e9e77ef1aec04baa76ac88cd3dc3
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-29 22:02:51 +0200

    Add new task

---
+++ ExamPrep/C# OOP Exam Regular  09 December 2023/Task1/Models/FreeDiver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Models
+{
+    public class FreeDiver : Diver
+    {
+
+        private const int freeDiverOxygenLevel = 120;
+        public FreeDiver(string name) : base(name, freeDiverOxygenLevel)
+        {
+        }
+
+        public override void Miss(int TimeToCatch)
+        {
+            double time = (double)TimeToCatch / 100 * 60;
+            this.OxygenLevel -= (int)Math.Round(time, MidpointRounding.AwayFromZero);
+            if (this.OxygenLevel <= 0)
+            {
+                OxygenLevel = 0;
+                UpdateHealthStatus();
+            }
+
+        }
+
+        public override void RenewOxy()
+        {
+            this.OxygenLevel = freeDiverOxygenLevel;
+        }
+    }
+}
