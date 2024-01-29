a86db0432c4144cd94b7172f8a71f351fed65593
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-29 22:02:41 +0200

    Add new task

---
+++ ExamPrep/C# OOP Exam Regular  09 December 2023/Task1/Models/Fish.cs
@@ -0,0 +1,66 @@
+﻿using NauticalCatchChallenge.Models.Contracts;
+using NauticalCatchChallenge.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Models
+{
+    public abstract class Fish : IFish
+    {
+        private string name;
+        private double points;
+
+        public Fish(string name, double points, int timeToCatch)
+        {
+            Name = name;
+            Points = points;
+            TimeToCatch = timeToCatch;
+        }
+        public string Name {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.FishNameNull);
+                }
+
+                name = value;
+            }
+        }
+
+        public double Points
+        {
+            get
+            {
+                return points;
+            }
+            private set
+            {
+                if (value < 1 || value > 10)
+                {
+                    throw new ArgumentException(ExceptionMessages.PointsNotInRange);
+                }
+
+                points = value;
+            }
+        }
+
+        public int TimeToCatch { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine($"{this.GetType().Name}: {Name} [ Points: {Points}, Time to Catch: {TimeToCatch} seconds ]");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
