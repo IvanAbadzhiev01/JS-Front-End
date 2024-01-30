f538a2a930182c65ee793f2e74a0d4e0e12d85c3
Ivan Abadjiev <107812744+IvanAbadjiev@users.noreply.github.com>
2024-01-30 19:10:16 +0200

    Add new task

---
+++ ExamPrep/C# OOP Exam Regular  09 December 2023/Task1/Repositories/FishRepository.cs
@@ -0,0 +1,30 @@
+﻿using NauticalCatchChallenge.Models.Contracts;
+using NauticalCatchChallenge.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Repositories
+{
+    public class FishRepository : IRepository<IFish>
+    {
+        private List<IFish> fish;
+        public FishRepository()
+        {
+            fish = new();
+        }
+        public IReadOnlyCollection<IFish> Models => fish;
+
+        public void AddModel(IFish model)
+        {
+            fish.Add(model);
+        }
+
+        public IFish GetModel(string name)
+        {
+            return fish.FirstOrDefault(f => f.Name == name);
+        }
+    }
+}
